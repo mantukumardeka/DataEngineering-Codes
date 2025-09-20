b8d05b87831741f11ded674f16775872ccd6fdec
“mantukumardeka” <mantukumardeka@gmail.com>
2025-09-20 11:04:43 +0530

    Added RDD and Interview

---
+++ PySparkCodes/RDD.py
@@ -0,0 +1,15 @@
+
+#Creating a RDD using PySpark
+
+from pyspark.sql import SparkSession
+from pyspark import SparkConf, SparkContext
+
+spark = SparkSession .builder .appName("Code") .getOrCreate()
+
+data=["Apple","Banana","Orange","Cherry",]
+
+rdd=spark.sparkContext.parallelize(data)
+print(rdd.collect())
+
+rdd1 = spark.sparkContext.textFile("/Users/mantukumardeka/Desktop/data/scdata.txt")
+print(rdd1.collect())
