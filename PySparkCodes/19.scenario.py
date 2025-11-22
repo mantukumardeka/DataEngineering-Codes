91996ef8d7e1ea172353d04327cf011a2682972b
“mantukumardeka” <mantukumardeka@gmail.com>
2025-11-22 12:34:25 +0530

    New File Added

---
+++ PySparkCodes/19.scenario.py
@@ -0,0 +1,42 @@
+import requests
+from pyspark.sql import SparkSession
+
+# Correct RAW JSON URL
+url = "https://raw.githubusercontent.com/mohankrishna02/interview-scenerios-spark-sql/master/Datasets/scen.json"
+
+local_path = "/Users/mantukumardeka/Desktop/DataEngineering/data_for_testing/complex1.json"
+
+print("Downloading JSON...")
+r = requests.get(url)
+
+# Save JSON file
+with open(local_path, "wb") as f:
+    f.write(r.content)
+
+print("Download complete.")
+
+# Start Spark
+spark = SparkSession.builder.appName("ScenarioData").getOrCreate()
+
+# Read JSON
+df = df = spark.read.format("json").option("multiline", "true").load(f"file://{local_path}")
+
+print("Schema:")
+
+
+from pyspark.sql.functions import *
+print("Data:")
+df.show(5, truncate=False)
+df.printSchema()
+finaldf = df.withColumn("multiMedia", explode(col("multiMedia"))).withColumn("dislikes",
+                                                                             expr("likeDislike.dislikes")).withColumn(
+    "likes", expr("likeDislike.likes")).withColumn("userAction", expr("likeDislike.userAction")).withColumn("createAt",
+                                                                                                            expr(
+                                                                                                                "multiMedia.createAt")).withColumn(
+    "description", expr("multiMedia.description")).withColumn("id", expr("multiMedia.id")).withColumn("likeCount", expr(
+    "multiMedia.likeCount")).withColumn("mediatype", expr("multiMedia.mediatype")).withColumn("name", expr(
+    "multiMedia.name")).withColumn("place", expr("multiMedia.place")).withColumn("url", expr("multiMedia.url")).drop(
+    "likeDislike", "multiMedia")
+print("flat Schema")
+finaldf.printSchema()
+finaldf.show()
--- PySparkCodes/work_and_delete.py
+++ PySparkCodes/work_and_delete.py
@@ -8,52 +8,10 @@ sc = SparkContext(conf=conf)
 sc.setLogLevel("ERROR")
 spark = SparkSession.builder.getOrCreate()
 
-# ------------------------
-# EMAIL MASKING UDF
-# m******t@gmail.com
-# ------------------------
-def mask_email(email):
-    try:
-        at_index = email.index("@")
-        first = email[0]              # first letter
-        last = email[at_index - 1]    # letter before @
-        stars = "*" * (at_index - 2)  # stars in between
-        return first + stars + last + email[at_index:]
-    except:
-        return email
 
-mask_email_udf = udf(mask_email, StringType())
 
-# ------------------------
-# MOBILE MASKING UDF
-# 9*****434
-# ------------------------
-def mask_mobile(mobile):
-    try:
-        first = mobile[0]
-        last3 = mobile[-3:]
-        stars = "*" * (len(mobile) - 4)
-        return first + stars + last3
-    except:
-        return mobile
+#df=spark.read.json("file:///Users/mantukumardeka/Desktop/DataEngineering/test.json")
 
-mask_mobile_udf = udf(mask_mobile, StringType())
+df = spark.read.format("json").option("multiline", "true").load("file:///Users/mantukumardeka/Desktop/DataEngineering/test.json")
+df.printSchema()
 
-# ------------------------
-# CREATE DATAFRAME
-# ------------------------
-df = spark.createDataFrame([
-    ("Renuka1992@gmail.com", "9856765434"),
-    ("anbu.arasu@gmail.com", "9844567788"),
-    ("mantuct@gmail.com", "9876543210")
-], ["email", "mobile"])
-
-df.show(truncate=False)
-
-# ------------------------
-# APPLY MASKING UDFs
-# ------------------------
-maskeddf = df.withColumn("email", mask_email_udf("email")) \
-             .withColumn("mobile", mask_mobile_udf("mobile"))
-
-maskeddf.show(truncate=False)
